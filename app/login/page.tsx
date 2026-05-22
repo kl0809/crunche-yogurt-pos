d011602458e35be2a6d22e8848588dcfde188878
Keliang <keliang200689@gmail.com>
2026-05-22 20:18:04 +0800

    fix reset password url

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -86,7 +86,7 @@ export default function LoginPage() {
         >
           Create Account
         </button>
-        
+
         <button
           onClick={resetPassword}
           className="w-full mt-3 bg-yellow-500 text-black px-4 py-2 rounded-xl"
